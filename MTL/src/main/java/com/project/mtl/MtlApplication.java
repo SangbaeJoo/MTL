2cb677bdb61f78ddc356bb716620dc455af93b9c
82103 <82103@DESKTOP-C23AKDH>
2021-02-21 23:35:39 +0900

    메인 프로젝트 업로드

---
+++ MTL/src/main/java/com/project/mtl/MtlApplication.java
@@ -0,0 +1,13 @@
+package com.project.mtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtlApplication.class, args);
+	}
+
+}
---
+++ MTL/src/test/java/com/project/mtl/MtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
